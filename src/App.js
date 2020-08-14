31c5f71ccfb662a68b0865228be276aaa23b1910
Riko Hermansyah <mr.rikohermansyah@gmail.com>
2020-08-14 17:30:48 +0700

    Square-playground

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Square from './Square.js';
 
 function App() {
   return (
@@ -19,7 +20,11 @@ function App() {
           Learn React
         </a>
       </header>
+      <Square size={50} />       
+      <Square initialColor="green" size={100} />
+      <Square initialColor="red" size={200} />
     </div>
+    //showTItle = {true/false} | you can insert this command after Square above if you want to use boolean
   );
 }
 
